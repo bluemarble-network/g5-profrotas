a939ae91ae08949fd1fffa25c5e4b3b73bd4b47b
bluemarble-network <bluemarblenetwork@gmail.com>
2023-07-15 18:26:19 -0300

    fix: yarn install --prod somente

--- src/utils/knex.ts
+++ src/utils/knex.ts
@@ -3,16 +3,11 @@ import 'dotenv/config'
 
 export const queryBuilder = knex({
   client: 'mysql',
-  connection: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT as string),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }
+  connection: process.env.DATABASE_URL,
+  pool: { min: 0, max: 4, idleTimeoutMillis: 60 * 1000 }
 })
 
-export async function insertOrUpdate (knex: any, tableName: string, data: any) {
+export async function insertOrUpdate(knex: any, tableName: string, data: any) {
   const firstData = data[0] ? data[0] : data
   return await knex.raw(
     knex(tableName).insert(data).toQuery() +
@@ -23,15 +18,17 @@ export async function insertOrUpdate (knex: any, tableName: string, data: any) {
   )
 }
 
-export async function insertOrUpdatetoQuery (knex: any, tableName: string, data: any) {
+export async function insertOrUpdatetoQuery(knex: any, tableName: string, data: any) {
   const firstData = data[0] ? data[0] : data
-  return await knex.raw(
-    knex(tableName).insert(data).toQuery() +
-      ' ON DUPLICATE KEY UPDATE ' +
-      Object.getOwnPropertyNames(firstData)
-        .map((field) => `${field}=VALUES(${field})`)
-        .join(', ')
-  ).toQuery()
+  return await knex
+    .raw(
+      knex(tableName).insert(data).toQuery() +
+        ' ON DUPLICATE KEY UPDATE ' +
+        Object.getOwnPropertyNames(firstData)
+          .map((field) => `${field}=VALUES(${field})`)
+          .join(', ')
+    )
+    .toQuery()
 }
 
 interface iLogDB {
@@ -40,6 +37,6 @@ interface iLogDB {
   qtdInserida?: number
 }
 
-export async function logDB (dadosLog: iLogDB) {
+export async function logDB(dadosLog: iLogDB) {
   return await queryBuilder('profrotas_log').insert(dadosLog)
 }
